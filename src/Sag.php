6439ed13587c60da9518ddae67818361290a6295
Sam Bisbee <sam@sbisbee.com>
2010-08-05 01:36:03 -0400

    We now support non-JSON data being returned to us, thereby fixing stand alone attachment GET'ing.

--- src/Sag.php
+++ src/Sag.php
@@ -560,11 +560,24 @@ class Sag
         $response->body .= $line;
     }
 
+    /*
+     * $json won't be set if invalid JSON is sent back to us. This will most
+     * likely happen if we're GET'ing an attachment that isn't JSON (ex., a
+     * picture or plain text). Don't be fooled by storing a PHP string in an
+     * attachment as text/plain and then expecting it to be parsed by
+     * json_decode().
+     */
     $json = json_decode($response->body);
-    if(!empty($json->error))
-      throw new SagCouchException("{$json->error} ({$json->reason})", $response->headers->_HTTP->status);
 
-    $response->body = ($this->decodeResp) ? $json : $response->body;
+    if(isset($json))
+    {
+      // Check for an error from CouchDB regardless of whether they want JSON
+      // returned.
+      if(!empty($json->error))
+        throw new SagCouchException("{$json->error} ({$json->reason})", $response->headers->_HTTP->status);
+
+      $response->body = ($this->decodeResp) ? $json : $response->body;
+    }
 
     return $response;
   }
