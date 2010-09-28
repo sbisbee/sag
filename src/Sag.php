9ff7ded0c380ca520c1a0c832c7f2cc8dee45e64
Sam Bisbee <sam@sbisbee.com>
2010-09-28 19:16:18 -0400

    Fixed bug in getAllDocs(). $incDocs is always true, since isset returns true if a variable is false and a null value throws an exception.
    Also fixed a strict warning for using an empty variable as an object.(cherry picked from commit 0b7733d9ce8a9f5182e17852c5e06a730cc8ad76)
    
    Not including the strict warning change. Not making the code uglier for the poor strict mode.

--- src/Sag.php
+++ src/Sag.php
@@ -276,7 +276,7 @@ class Sag
 
     $qry = array();
 
-    if(isset($incDocs))
+    if($incDocs !== false)
     {
       if(!is_bool($incDocs))
         throw new SagException('getAllDocs() expected a boolean for include_docs.');
