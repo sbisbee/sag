c9bfbe549dd52da0d2f1db7602b91c458dfa5c63
Sam Bisbee <sam@sbisbee.com>
2010-11-17 00:24:27 -0500

    Test for item creation in mem cache.

---
+++ tests/SagMemoryCacheTest.php
@@ -0,0 +1,55 @@
+<?php
+/*
+  Copyright 2010 Sam Bisbee 
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+require_once('PHPUnit/Framework.php');
+require_once('../src/SagMemoryCache.php');
+
+class SagMemoryCacheTest extends PHPUnit_Framework_TestCase
+{
+  protected $cache;
+
+  public function setUp()
+  {
+    $this->cache = new SagMemoryCache();
+  }
+
+  public function test_createAndGet()
+  {
+    $prevSize = $this->cache->getUsage();
+
+    $url = '/bwah';
+
+    $item = new StdClass();
+    $item->body = new StdClass();
+    $item->body->foo = "bar";
+    $item->headers = new StdClass();
+    $item->headers->Etag = "\"asdfasfsadfsadf\"";
+
+    $res = $this->cache->set($url, $item);
+    $this->assertTrue($res === true || is_object($res));
+
+    $fromCache = $this->cache->get($url);
+
+    $this->assertEquals($item, $fromCache);
+
+    //make sure we aren't doing any funky pointer stuff
+    unset($item);
+    $this->assertTrue(is_object($fromCache));
+    $this->assertTrue(is_object($this->cache->get($url)));
+  }
+}
+?>
