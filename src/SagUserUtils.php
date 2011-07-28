8a3b23387d29dbd15faad188536da8691d258ab8
Sam Bisbee <sam@sbisbee.com>
2011-07-27 23:51:04 -0400

    Licensing and class doc comments.

--- src/SagUserUtils.php
+++ src/SagUserUtils.php
@@ -1,6 +1,24 @@
 <?php
-require('Sag.php');
+/*
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
 
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+require_once('Sag.php');
+
+/**
+ * Utilities for creating, managing, and removing users in CouchDB's _users
+ * database.
+ */
 class SagUserUtils
 {
   private static $USER_ID_PREFIX = 'org.couchdb.user';
