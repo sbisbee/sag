3fb114ce6fab53fc901db8396731cbc54598af22
Sam Bisbee <sam@sbisbee.com>
2011-07-28 12:02:04 -0400

    First pass at implementing adapters. Isn't complete, but I'm on a plane without access to PHP docs.

--- src/Sag.php
+++ src/Sag.php
@@ -16,6 +16,7 @@
 require_once('SagException.php');
 require_once('SagCouchException.php');
 require_once('SagConfigurationCheck.php');
+require_once('SagNativeHTTPAdapter.php');
 
 /**
  * The Sag class provides the core functionality for talking to CouchDB.
@@ -56,30 +57,28 @@ class Sag
 
   private $staleDefault;                //Whether or not to use ?stale=ok on all design doc calls
 
-  private $connPool = array();          //Connection pool
-
   private $globalCookies = array();
 
+  private $httpAdapter;
+
   /**
-   * @param string $host The host's IP or address of the Couch we're connecting
-   * to.
-   * @param string $port The host's port that Couch is listening on.
+   * @param string $host (OPTIONAL) The host's IP or address of the Couch we're
+   * connecting to. Defaults to '127.0.0.1'.
+   *
+   * @param string $port (OPTIONAL) The host's port that Couch is listening on.
+   * Defaults to '5984'.
+   *
+   * @param SagHTTPAdapter $httpAdapter (OPTIONAL) An implementation of the
+   * SagHTTPAdapter. Defaults to SagNativeHTTPAdapter.
    */
-  public function __construct($host = "127.0.0.1", $port = "5984")
+  public function __construct($host = "127.0.0.1", $port = "5984", $httpAdapter = null)
   {
     SagConfigurationCheck::run();
 
     $this->host = $host;
     $this->port = $port;
-  }
 
-  /**
-   * Closes any sockets that are left open in the connection pool.
-   */
-  public function __destruct()
-  {
-    foreach($this->connPool as $sock)
-      @fclose($sock);
+    $this->httpAdapter = ($httpAdapter) ?: new SagNativeHTTPAdapter($host, $port);
   }
 
   /**
@@ -906,171 +905,9 @@ class Sag
       $headers['Content-Type'] = 'application/json';
 
     if($data)
-      $headers['Content-Length'] = strlen($data); 
+      $headers['Content-Length'] = strlen($data);
 
-    $buff = "$method $url HTTP/1.0\r\n";
-    foreach($headers as $k => $v)
-      $buff .= "$k: $v\r\n";
-
-    $buff .= "\r\n$data"; //it's okay if $data isn't set
-
-    if($data && $method !== "PUT")
-      $buff .= "\r\n\r\n";
-
-    // Open the socket only once we know everything is ready and valid.
-    $sock = null;
-
-    while(!$sock)
-    {
-      if(sizeof($this->connPool) > 0)
-      {
-        $maybeSock = array_shift($this->connPool);
-        $meta = stream_get_meta_data($maybeSock);
-
-        if(!$meta['timed_out'] && !$meta['eof'])
-          $sock = $maybeSock;
-        elseif(is_resource($maybeSock))
-          fclose($maybeSock);
-      }
-      else
-      {
-        try
-        {
-          //these calls should throw on error
-          if($this->socketOpenTimeout)
-            $sock = fsockopen($this->host, $this->port, $sockErrNo, $sockErrStr, $this->socketOpenTimeout);
-          else
-            $sock = fsockopen($this->host, $this->port, $sockErrNo, $sockErrStr);
-
-          //some PHP configurations don't throw when fsockopen() fails
-          if(!$sock) {
-            throw Exception($sockErrStr, $sockErrNo);
-          }
-        }
-        catch(Exception $e)
-        {
-          throw new SagException('Was unable to fsockopen() a new socket: '.$e->getMessage());
-        }
-      }
-    }
-
-    if(!$sock)
-      throw new SagException("Error connecting to {$this->host}:{$this->port} - $sockErrStr ($sockErrNo).");
-
-    // Send the packet.
-    fwrite($sock, $buff);
-
-    // Set the timeout.
-    if(isset($this->socketRWTimeoutSeconds))
-      stream_set_timeout($sock, $this->socketRWTimeoutSeconds, $this->socketRWTimeoutMicroseconds);
-
-    // Prepare the data structure to store the response.
-    $response = new StdClass();
-    $response->headers = new StdClass();
-    $response->headers->_HTTP = new StdClass();
-    $response->body = '';
-
-    $isHeader = true;
-    $sockInfo = stream_get_meta_data($sock);
-
-    // Read in the response.
-    while(
-      !feof($sock) && 
-      (
-        $isHeader ||
-        (
-          !$isHeader &&
-          $method != 'HEAD' &&
-          (
-            !isset($response->headers->{'Content-Length'}) ||
-            (
-              isset($response->headers->{'Content-Length'}) &&
-              strlen($response->body) < $response->headers->{'Content-Length'}
-            )
-          )
-        )
-      )
-    )
-    {
-      if($sockInfo['timed_out'])
-        throw new SagException('Connection timed out while reading.');
-
-      //TODO deal with fgets() returning false
-      //TODO add tests to check binary safeness
-      $line = fgets($sock);
-
-      if($isHeader)
-      {
-        $line = trim($line);
-
-        if($isHeader && empty($line))
-          $isHeader = false; //the delim blank line
-        else
-        {
-          if(!isset($response->headers->_HTTP->raw))
-          {
-            //the first header line is always the HTTP info
-            $response->headers->_HTTP->raw = $line;
-
-            if(preg_match('(^HTTP/(?P<version>\d+\.\d+)\s+(?P<status>\d+))S', $line, $match))
-            {
-              $response->headers->_HTTP->version = $match['version'];
-              $response->headers->_HTTP->status = $match['status'];
-              $response->status = $match['status'];
-            }
-            else
-              throw new SagException('There was a problem while handling the HTTP protocol.'); //whoops!
-          }
-          else
-          {
-            $line = explode(':', $line, 2);
-            $response->headers->$line[0] = ltrim($line[1]);
-
-            if($line[0] == 'Set-Cookie')
-            {
-              $response->cookies = new StdClass();
-
-              foreach(explode('; ', $line[1]) as $cookie)
-              {
-                $crumbs = explode('=', $cookie);
-                $response->cookies->{trim($crumbs[0])} = trim($crumbs[1]);
-              } 
-            }
-          }
-        }
-      }
-      else
-        $response->body .= $line;
-    }
-
-    //We're done with the socket, so someone else can use it.
-    if($response->headers->Connection == 'Keep-Alive')
-      $this->connPool[] = $sock;
-
-    //Make sure we got the complete response.
-    if($method != 'HEAD' && isset($response->headers->{'Content-Length'}) && strlen($response->body) < $response->headers->{'Content-Length'})
-      throw new SagException('Unexpected end of packet.');
-
-    /*
-     * $json won't be set if invalid JSON is sent back to us. This will most
-     * likely happen if we're GET'ing an attachment that isn't JSON (ex., a
-     * picture or plain text). Don't be fooled by storing a PHP string in an
-     * attachment as text/plain and then expecting it to be parsed by
-     * json_decode().
-     */
-    $json = json_decode($response->body);
-
-    if(isset($json))
-    {
-      // Check for an error from CouchDB regardless of whether they want JSON
-      // returned.
-      if(!empty($json->error))
-        throw new SagCouchException("{$json->error} ({$json->reason})", $response->headers->_HTTP->status);
-
-      $response->body = ($this->decodeResp) ? $json : $response->body;
-    }
-
-    return $response;
+    return $this->httpAdapter->procPacket($method, $url, $data, $headers);
   }
 
   /**
---
+++ src/SagHTTPAdapter.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Provides a common interface for Sag to connect to CouchDB over HTTP,
+ * allowing for different drivers to be used thereby controling your project's
+ * dependencies.
+ *
+ * @version 0.6.0
+ * @package HTTP
+ */
+abstract class SagHTTPAdapter {
+  public $decodeJSON = true;
+
+  protected $host;
+  protected $port;
+
+  public function __construct($host = "127.0.0.1", $port = "5984") {
+    $this->host = $host;
+    $this->port = $port;
+  }
+
+  /**
+   * Processes the packet, returning the server's response.
+   */
+  abstract public function procPacket($method, $url, $data = null, $headers = array());
+
+  private function makeResult($response) {
+    /*
+     * $json won't be set if invalid JSON is sent back to us. This will most
+     * likely happen if we're GET'ing an attachment that isn't JSON (ex., a
+     * picture or plain text). Don't be fooled by storing a PHP string in an
+     * attachment as text/plain and then expecting it to be parsed by
+     * json_decode().
+     */
+    $json = json_decode($response->body);
+
+    if(isset($json))
+    {
+      // Check for an error from CouchDB regardless of whether they want JSON
+      // returned.
+      if(!empty($json->error))
+        throw new SagCouchException("{$json->error} ({$json->reason})", $response->headers->_HTTP->status);
+
+      $response->body = ($this->decodeResp) ? $json : $response->body;
+    }
+
+    return $response;
+  }
+}
+?>
