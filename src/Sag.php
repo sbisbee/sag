12780290cbadd27560bc2e3d714bc1ec07ba9d3e
Sam Bisbee <sam@sbisbee.com>
2011-08-26 00:52:00 -0400

    Fixing a bug where HTTP response codes >=400 for a HEAD reqeust were not throwing a SagCouchException - they would just return a Sag response with error codes in the header and an empty body. Now instead of checking the body for errors, we first check to see if the request was a HEAD and make sure the HTTP status <400. Then we just return the response, thereby circumventing the body parsing logic.
    Side note, apparently json_decode(null) takes a few miliseconds longer than
    json_decode('{"a": "hi"}')

--- src/Sag.php
+++ src/Sag.php
@@ -1093,22 +1093,42 @@ class Sag {
     }
 
     /*
-     * $json won't be set if invalid JSON is sent back to us. This will most
-     * likely happen if we're GET'ing an attachment that isn't JSON (ex., a
-     * picture or plain text). Don't be fooled by storing a PHP string in an
-     * attachment as text/plain and then expecting it to be parsed by
-     * json_decode().
+     * HEAD requests can return an HTTP response code >=400, meaning that there
+     * was a CouchDB error, but we don't get a $response->body->error because
+     * HEAD responses don't have bodies.
+     *
+     * And we do this before the json_decode() because even running
+     * json_decode() on undefined can take longer than calling it on a JSON
+     * string. So no need to run any of the $json code.
      */
-    $json = json_decode($response->body);
-
-    if(isset($json)) {
-      // Check for an error from CouchDB regardless of whether they want JSON
-      // returned.
-      if(!empty($json->error)) {
-        throw new SagCouchException("{$json->error} ({$json->reason})", $response->headers->_HTTP->status);
+    if($method == 'HEAD') {
+      if($response->headers->_HTTP->status >= 400) {
+        throw new SagCouchException('HTTP/CouchDB error without message body', $response->headers->_HTTP->status);
       }
 
-      $response->body = ($this->decodeResp) ? $json : $response->body;
+      //no else needed - just going to return below
+    }
+    else {
+      /*
+       * $json won't be set if invalid JSON is sent back to us. This will most
+       * likely happen if we're GET'ing an attachment that isn't JSON (ex., a
+       * picture or plain text). Don't be fooled by storing a PHP string in an
+       * attachment as text/plain and then expecting it to be parsed by
+       * json_decode().
+       */
+      $json = json_decode($response->body);
+
+      if(isset($json)) {
+        /*
+         * Check for an error from CouchDB regardless of whether they want JSON
+         * returned.
+         */
+        if(!empty($json->error)) {
+          throw new SagCouchException("{$json->error} ({$json->reason})", $response->headers->_HTTP->status);
+        }
+
+        $response->body = ($this->decodeResp) ? $json : $response->body;
+      }
     }
 
     return $response;
