39ff84c89c43b876a637bb1539c80f6d4b5ac2e7
Jerry Ablan <jerryablan@gmail.com>
2011-11-08 12:42:34 -0500

    Updated CURL handler

--- src/httpAdapters/SagHTTPAdapter.php
+++ src/httpAdapters/SagHTTPAdapter.php
@@ -1,206 +1,298 @@
 <?php
 /**
  * Provides a common interface for Sag to connect to CouchDB over HTTP,
- * allowing for different drivers to be used thereby controling your project's
+ * allowing for different drivers to be used thereby controlling your project's
  * dependencies.
  *
- * @version 0.7.0
+ * @version 0.7.1
  * @package HTTP
  */
-abstract class SagHTTPAdapter {
-  public $decodeResp = true;
+abstract class SagHTTPAdapter
+{
+	//*************************************************************************
+	//* Private Members
+	//*************************************************************************
 
-  protected $host;
-  protected $port;
+	/**
+	 * @var bool
+	 */
+	public $decodeResp = true;
+	/**
+	 * @var string
+	 */
+	protected $host = '127.0.0.1';
+	/**
+	 * @var int
+	 */
+	protected $port = 5984;
+	/**
+	 * @var string http or https
+	 */
+	protected $proto = 'http';
+	/**
+	 * @var string
+	 */
+	protected $sslCertPath = null;
+	/**
+	 * @var int The seconds until socket connection timeout
+	 */
+	protected $socketOpenTimeout;
+	/**
+	 * @var int The seconds for socket I/O timeout
+	 */
+	protected $socketRWTimeoutSeconds;
+	/**
+	 * @var int The microseconds for socket I/O timeout
+	 */
+	protected $socketRWTimeoutMicroseconds;
+	/**
+	 * @var bool If true, no body will be returned for a HEAD request
+	 */
+	protected $_suppressHeadBody;
 
-  protected $proto = 'http'; //http or https
-  protected $sslCertPath;
+	//*************************************************************************
+	//* Public Methods
+	//*************************************************************************
 
-  protected $socketOpenTimeout;                 //The seconds until socket connection timeout
-  protected $socketRWTimeoutSeconds;            //The seconds for socket I/O timeout
-  protected $socketRWTimeoutMicroseconds;       //The microseconds for socket I/O timeout
+	/**
+	 * @param string $host
+	 * @param string $port
+	 */
+	public function __construct( $host = '127.0.0.1', $port = '5984' )
+	{
+		$this->host = $host;
+		$this->port = $port;
+	}
 
-  public function __construct($host = "127.0.0.1", $port = "5984") {
-    $this->host = $host;
-    $this->port = $port;
-  }
+	/**
+	 * @param \stdClass $response
+	 * @param string $method
+	 * @return \stdClass
+	 * @throws SagCouchException|SagException
+	 */
+	protected function makeResult( $response, $method )
+	{
+		if ( 'HEAD' == $method )
+		{
+			/**
+			 * HEAD requests can return an HTTP response code >= 400, meaning that there
+			 * was a CouchDB error, but we don't get a $response->body->error because
+			 * HEAD responses don't have bodies.
+			 *
+			 * And we do this before the json_decode() because even running
+			 * json_decode() on undefined can take longer than calling it on a JSON
+			 * string. So no need to run any of the $_jsonResponse code.
+			 */
+			if ( $response->headers->_HTTP->status >= 400 )
+			{
+				throw new SagCouchException( 'HTTP/CouchDB error without message body', $response->headers->_HTTP->status );
+			}
 
-  protected function makeResult($response, $method) {
-    //Make sure we got the complete response.
-    if(
-      $method != 'HEAD' &&
-      isset($response->headers->{'Content-Length'}) &&
-      strlen($response->body) != $response->headers->{'Content-Length'}
-    ) {
-      throw new SagException('Unexpected end of packet.');
-    }
+			//	And that's it!
+			return $response;
+		}
 
-    /*
-     * HEAD requests can return an HTTP response code >=400, meaning that there
-     * was a CouchDB error, but we don't get a $response->body->error because
-     * HEAD responses don't have bodies.
-     *
-     * And we do this before the json_decode() because even running
-     * json_decode() on undefined can take longer than calling it on a JSON
-     * string. So no need to run any of the $json code.
-     */
-    if($method == 'HEAD') {
-      if($response->headers->_HTTP->status >= 400) {
-        throw new SagCouchException('HTTP/CouchDB error without message body', $response->headers->_HTTP->status);
-      }
+		//	Make sure we have entire response
+		if ( isset( $response->headers->{'Content-Length'} ) && strlen( $response->body ) != $response->headers->{'Content-Length'} )
+		{
+			throw new SagException( 'Content length does not match size of received body.' );
+		}
 
-      //no else needed - just going to return below
-    }
-    else {
-      /*
-       * $json won't be set if invalid JSON is sent back to us. This will most
-       * likely happen if we're GET'ing an attachment that isn't JSON (ex., a
-       * picture or plain text). Don't be fooled by storing a PHP string in an
-       * attachment as text/plain and then expecting it to be parsed by
-       * json_decode().
-       */
-      $json = json_decode($response->body);
+		/**
+		 * $_jsonResponse will be set to false if invalid JSON is sent back to us.
+		 * This will most likely happen if we're GET'ing an attachment that isn't JSON
+		 * (ex., a picture or plain text). Don't be fooled by storing a PHP string in an
+		 * attachment as text/plain and then expecting it to be parsed properly by json_decode().
+		 */
+		if ( false !== ( $_jsonResponse = json_decode( $response->body ) ) )
+		{
+			/**
+			 * Check for an error from CouchDB regardless of whether they want JSON returned.
+			 */
+			if ( !empty( $_jsonResponse->error ) )
+			{
+				throw new SagCouchException( $_jsonResponse->error . ' (' . $_jsonResponse->reason . ')"', $response->headers->_HTTP->status );
+			}
 
-      if(isset($json)) {
-        /*
-         * Check for an error from CouchDB regardless of whether they want JSON
-         * returned.
-         */
-        if(!empty($json->error)) {
-          throw new SagCouchException("{$json->error} ({$json->reason})", $response->headers->_HTTP->status);
-        }
+			$response->body = ( $this->decodeResp ? $_jsonResponse : $response->body );
+		}
 
-        $response->body = ($this->decodeResp) ? $json : $response->body;
-      }
-    }
+		return $response;
+	}
 
-    return $response;
-  }
+	/**
+	 * @param string $cookieHeader
+	 * @return \stdClass
+	 */
+	protected function parseCookieString( $cookieHeader )
+	{
+		$_cookies = new \stdClass();
 
-  protected function parseCookieString($cookieStr) {
-    $cookies = new stdClass();
+		foreach ( explode( '; ', $cookieHeader ) as $_cookie )
+		{
+			$_crumbs = explode( '=', $_cookie );
+			$_cookies->{trim( $_crumbs[0] )} = trim( $_crumbs[1] );
+		}
 
-    foreach(explode('; ', $cookieStr) as $cookie) {
-      $crumbs = explode('=', $cookie);
-      $cookies->{trim($crumbs[0])} = trim($crumbs[1]);
-    }
+		return $_cookies;
+	}
 
-    return $cookies;
-  }
+	/**
+	 * Processes the packet, returning the server's response.
+	 *
+	 * @param string $method
+	 * @param string $url
+	 * @param mixed|null $data
+	 * @param array $headers
+	 */
+	abstract public function procPacket( $method, $url, $data = null, $headers = array() );
 
-  /**
-   * Processes the packet, returning the server's response.
-   */
-  abstract public function procPacket($method, $url, $data = null, $headers = array());
+	//*************************************************************************
+	//* Properties
+	//*************************************************************************
 
-  /**
-   * Whether to use HTTPS or not.
-   */
-  public function useSSL($use) {
-    $this->proto = 'http' . (($use) ? 's' : '');
-  }
+	/**
+	 * Whether to use HTTPS or not.
+	 *
+	 * @param boolean $use
+	 */
+	public function useSSL( $use = false )
+	{
+		$this->proto = 'http' . ( ( $use ) ? 's' : '' );
+	}
 
-  /**
-   * Sets the location of the CA file.
-   */
-  public function setSSLCert($path) {
-    $this->sslCertPath = $path;
-  }
+	/**
+	 * Sets the location of the CA file.
+	 *
+	 * @param $path
+	 */
+	public function setSSLCert( $path = null )
+	{
+		$this->sslCertPath = $path;
+	}
 
-  /**
-   * Returns whether Sag is using SSL.
-   */
-  public function usingSSL() {
-    return $this->proto === 'https';
-  }
+	/**
+	 * Returns whether Sag is using SSL.
+	 *
+	 * @return bool
+	 */
+	public function usingSSL()
+	{
+		return ( 'https' == $this->proto );
+	}
 
-  /**
-   * Sets how long Sag should wait to establish a connection to CouchDB.
-   *
-   * @param int $seconds
-   */
-  public function setOpenTimeout($seconds) {
-    if(!is_int($seconds) || $seconds < 1) {
-      throw new SagException('setOpenTimeout() expects a positive integer.');
-    }
+	/**
+	 * Sets how long Sag should wait to establish a connection to CouchDB.
+	 *
+	 * @param int $seconds
+	 */
+	public function setOpenTimeout( $seconds )
+	{
+		if ( !is_int( $seconds ) || $seconds < 1 )
+		{
+			throw new SagException( 'setOpenTimeout() expects a positive integer.' );
+		}
 
-    $this->socketOpenTimeout = $seconds;
-  }
+		$this->socketOpenTimeout = $seconds;
+	}
 
-  /**
-   * Set how long we should wait for an HTTP request to be executed.
-   *
-   * @param int $seconds The number of seconds.
-   * @param int $microseconds The number of microseconds.
-   */
-  public function setRWTimeout($seconds, $microseconds) {
-    if(!is_int($microseconds) || $microseconds < 0) {
-      throw new SagException('setRWTimeout() expects $microseconds to be an integer >= 0.');
-    }
+	/**
+	 * Set how long we should wait for an HTTP request to be executed.
+	 *
+	 * @param int $seconds The number of seconds.
+	 * @param int $microseconds The number of microseconds.
+	 */
+	public function setRWTimeout( $seconds, $microseconds = 0 )
+	{
+		if ( !is_int( $microseconds ) || $microseconds < 0 )
+		{
+			throw new SagException( 'setRWTimeout() expects $microseconds to be an integer >= 0.' );
+		}
 
-    //TODO make this better, including checking $microseconds
-    //$seconds can be 0 if $microseconds > 0
-    if(
-      !is_int($seconds) ||
-      (
-        (!$microseconds && $seconds < 1) ||
-        ($microseconds && $seconds < 0)
-      )
-    ) {
-      throw new SagException('setRWTimeout() expects $seconds to be a positive integer.');
-    }
+		//TODO make this better, including checking $microseconds
+		//$seconds can be 0 if $microseconds > 0
+		if ( !is_int( $seconds ) || ( ( !$microseconds && $seconds < 1 ) || ( $microseconds && $seconds < 0 ) )
+		)
+		{
+			throw new SagException( 'setRWTimeout() expects $seconds to be a positive integer.' );
+		}
 
-    $this->socketRWTimeoutSeconds = $seconds;
-    $this->socketRWTimeoutMicroseconds = $microseconds;
-  }
+		$this->socketRWTimeoutSeconds = $seconds;
+		$this->socketRWTimeoutMicroseconds = $microseconds;
+	}
 
-  /**
-   * Returns an associative array of the currently set timeout values.
-   *
-   * @return array An associative array with the keys 'open', 'rwSeconds', and
-   * 'rwMicroseconds'.
-   *
-   * @see setTimeoutsFromArray()
-   */
-  public function getTimeouts() {
-    return array(
-      'open' => $this->socketOpenTimeout,
-      'rwSeconds' => $this->socketRWTimeoutSeconds,
-      'rwMicroseconds' => $this->socketRWTimeoutMicroseconds
-    );
-  }
+	/**
+	 * Returns an associative array of the currently set timeout values.
+	 *
+	 * @return array An associative array with the keys 'open', 'rwSeconds', and
+	 * 'rwMicroseconds'.
+	 *
+	 * @see setTimeoutsFromArray()
+	 */
+	public function getTimeouts()
+	{
+		return array(
+			'open' => $this->socketOpenTimeout,
+			'rwSeconds' => $this->socketRWTimeoutSeconds,
+			'rwMicroseconds' => $this->socketRWTimeoutMicroseconds
+		);
+	}
 
-  /**
-   * A utility function that sets the different timeout values based on an
-   * associative array.
-   *
-   * @param array $arr An associative array with the keys 'open', 'rwSeconds',
-   * and 'rwMicroseconds'.
-   *
-   * @see getTimeouts()
-   */
-  public function setTimeoutsFromArray($arr) {
-    /*
-     * Validation is lax in here because this should only ever be used with
-     * getTimeouts() return values. If people are using it by hand then there
-     * might be something wrong with the API.
-     */
-    if(!is_array($arr)) {
-      throw SagException('Expected an array and got something else.');
-    }
+	/**
+	 * A utility function that sets the different timeout values based on an
+	 * associative array.
+	 *
+	 * @param array $arr An associative array with the keys 'open', 'rwSeconds',
+	 * and 'rwMicroseconds'.
+	 *
+	 * @see getTimeouts()
+	 */
+	public function setTimeoutsFromArray( $arr )
+	{
+		/**
+		 * Validation is lax in here because this should only ever be used with
+		 * getTimeouts() return values. If people are using it by hand then there
+		 * might be something wrong with the API.
+		 */
+		if ( !is_array( $arr ) )
+		{
+			throw new SagException( 'Expected an array and got something else.' );
+		}
 
-    if(is_int($arr['open'])) {
-      $this->setOpenTimeout($arr['open']);
-    }
+		if ( is_int( $arr['open'] ) )
+		{
+			$this->setOpenTimeout( $arr['open'] );
+		}
 
-    if(is_int($arr['rwSeconds'])) {
-      if(is_int($arr['rwMicroseconds'])) {
-        $this->setRWTimeout($arr['rwSeconds'], $arr['rwMicroseconds']);
-      }
-      else {
-        $this->setRWTimeout($arr['rwSeconds']);
-      }
-    }
-  }
+		if ( is_int( $arr['rwSeconds'] ) )
+		{
+			if ( is_int( $arr['rwMicroseconds'] ) )
+			{
+				$this->setRWTimeout( $arr['rwSeconds'], $arr['rwMicroseconds'] );
+			}
+			else
+			{
+				$this->setRWTimeout( $arr['rwSeconds'] );
+			}
+		}
+	}
+
+	/**
+	 * @param boolean $suppressHeadBody
+	 * @return \SagHTTPAdapter
+	 */
+	public function setSuppressHeadBody( $suppressHeadBody )
+	{
+		$this->_suppressHeadBody = $suppressHeadBody;
+		return $this;
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function getSuppressHeadBody()
+	{
+		return $this->_suppressHeadBody;
+	}
+
 }
-?>
