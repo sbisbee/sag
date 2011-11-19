a3b0e46a31219fed18510957f36af3a73d6a4924
David Hardtke <david@bright.com>
2011-11-19 03:26:23 +0300

    use property_exists and isset methods to avoid PHP error messages

--- src/httpAdapters/SagNativeHTTPAdapter.php
+++ src/httpAdapters/SagNativeHTTPAdapter.php
@@ -187,7 +187,7 @@ class SagNativeHTTPAdapter extends SagHTTPAdapter {
           }
         }
       }
-      else if($response->headers->{'Transfer-Encoding'}) {
+      else if(property_exists($response->headers,'Transfer-Encoding')) {
         /*
          * Parse the response's body, which is being sent in chunks. Welcome to
          * HTTP/1.1 land.
@@ -252,7 +252,7 @@ class SagNativeHTTPAdapter extends SagHTTPAdapter {
     }
 
     // HTTP/1.1 assumes persisted connections, but proxies might close them.
-    if(strtolower($response->headers->Connection) != 'close') {
+    if(property_exists($response->headers,'Connection') && strtolower($response->headers->Connection) != 'close') {
       $this->connPool[] = $sock;
     }
 
