247f7f4e2785e69e6512e9785dc8c62e43a2d5b0
Sam Bisbee <sam@sbisbee.com>
2012-07-31 14:14:28 -0400

    Removing old and broken Continue header handling code now that we prevent them.

--- src/httpAdapters/SagCURLHTTPAdapter.php
+++ src/httpAdapters/SagCURLHTTPAdapter.php
@@ -93,18 +93,7 @@ class SagCURLHTTPAdapter extends SagHTTPAdapter {
       $response->body = '';
 
       // split headers and body
-      list($continue, $headers, $response->body) = explode("\r\n\r\n", $chResponse);
-
-      /*
-       * It doesn't always happen, but it seems that we will sometimes get a
-       * Continue header that will screw parsing up.
-       */
-      if(!$response->body) {
-        $response->body = $headers;
-        $headers = $continue;
-      }
-
-      unset($continue);
+      list($headers, $response->body) = explode("\r\n\r\n", $chResponse);
 
       // split up the headers
       $headers = explode("\r\n", $headers);
