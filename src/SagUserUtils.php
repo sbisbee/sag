ac2fb850770c17331e8d23baef098228358fa8ed
Rob Porter <rob@weeverapps.com>
2014-12-15 15:16:44 -0500

    Fixes broken changePassword method for user accounts using PBKDF2 hashes (couch 1.3.0+)

--- src/SagUserUtils.php
+++ src/SagUserUtils.php
@@ -151,18 +151,24 @@ class SagUserUtils {
       throw new SagException('This does not look like a document: there is no _id.');
     }
 
-    if(empty($doc->salt) || empty($doc->password_sha)) {
-      throw new SagException('This does not look like a user or it is an admin. Change admin passwords via the server config.');
-    }
-
     if(empty($newPassword)) {
       throw new SagException('Empty password are not allowed.');
     }
 
-    $salt = self::makeSalt();
+    if(empty($doc->salt) || empty($doc->password_sha)) {
+      if( empty($doc->iterations) ) {
+        throw new SagException('This does not look like a user or it is an admin. Change admin passwords via the server config.');
+      } else {
+        /* This is CouchDB 1.3.0+; it automatically takes a plaintext password and converts it to PBKDF2 */
+        $doc->password = $newPassword;
+      }
+    } else {
 
-    $doc->password_sha = sha1($newPassword . $salt);
-    $doc->salt = $salt;
+      $salt = self::makeSalt();
+
+      $doc->password_sha = sha1($newPassword . $salt);
+      $doc->salt = $salt;
+    }
 
     return ($upload) ? $this->sag->put($doc->_id, $doc) : $doc;
   }
